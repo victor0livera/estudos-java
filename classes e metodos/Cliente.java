05c7d90d149708a9be66a542856ac3d8a344d888
Victor <jv81332@gmail.com>
2024-12-30 23:39:36 -0300

    Cliente.java

---
+++ classes e metodos/Cliente.java
@@ -0,0 +1,41 @@
+public class Cliente {
+    private int codigo;
+    private String nome;
+    private String endereco;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public void cadastrarEndereco(String endereco) {
+        this.endereco = "Rua teste";
+    }
+
+    public String retornarEndereco() {
+        return "endereco atualizado";
+    }
+
+    public int valorTotal() {
+        return 2;
+    }
+}
