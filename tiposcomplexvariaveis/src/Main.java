3d09bad68e630487d8c023821f5081cdabd8d5c9
Victor <jv81332@gmail.com>
2025-01-15 20:36:23 -0300

    tiposcomplexvariaveis

---
+++ tiposcomplexvariaveis/src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    /*
+    Tipos complexos (Wrappers)
+    boolean---------Boolean
+    byte---------Byte
+    char---------Character
+    short---------Short
+    int---------Integer
+    long---------Long
+    float---------Float
+    double---------Double
+
+    diferenca entre tipos complexos e primitivos: os complexos tem metodos auxiliares
+     */
+    private int codigo1;
+    private Integer codigo;
+    private Long codigoMaior= 12040l;
+    private Double valorDecimal= 10.1;
+    private Boolean status = false;
+    private Float valorDecimal1= 10.0f;
+    private Short shor;
+    private Byte bi;
+    private Character letra = 'A';
+
+    public static void main(String args[]) {
+
+    }
+
+    public void teste() {
+        this.codigo = null;
+    }
+}
