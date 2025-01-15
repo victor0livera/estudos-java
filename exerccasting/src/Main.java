c2c2fe9b618c130eff65ff48d932a4403148ae40
Victor <jv81332@gmail.com>
2025-01-15 20:40:00 -0300

    exerccasting

---
+++ exerccasting/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        //primitivo
+        int num = 1;
+
+        //tranformando wrapper em primitivo
+        Character fala = 'a';
+
+        //transformando tipo primitivo em wrapper (unboxing)
+        num = Integer.valueOf(1);
+    }
+}
