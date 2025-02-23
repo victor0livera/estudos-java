be850a1af536624b95146bc8f335a7ea61e3f447
Victor <jv81332@gmail.com>
2025-02-23 13:02:44 -0300

    colecoesll

---
+++ colecoesll/src/arrayll/ExemploBubbleSortFixo.java
@@ -0,0 +1,29 @@
+package arrayll;
+
+public class ExemploBubbleSortFixo {
+    public static void main(String[] args) {
+        int [] vet = {8,9,3,5,1};
+        int aux = 0;
+        int i = 0;
+
+        System.out.println("valor desordenado: ");
+        for (i = 0; i<5; i++) {
+            System.out.println(vet[i]);
+        }
+        System.out.println(" ");
+
+        for (i = 0; i<5; i++){
+            for (int j = 0; j<4; j++){
+                if(vet[j] > vet[j + 1]){
+                    aux = vet[j];
+                    vet[j] = vet[j + 1];
+                    vet[j + 1] = aux;
+                }
+            }
+        }
+        System.out.println("vetor organizado: ");
+        for (i = 0; i<5; i++) {
+            System.out.println(vet[i]);
+        }
+    }
+}
